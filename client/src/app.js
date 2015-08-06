f1538764eb7b791b1382d55e53e967aad053e97a
chikeichan <chikeichan@gmail.com>
2015-08-06 01:23:23 -0700

    Experimenting new Renderer API

--- client/src/app.js
+++ client/src/app.js
@@ -9,13 +9,44 @@ Trio.Module.import({
 })
 
 .then(function(ret) {
-    var layout = ret.layoutModule;
-    var playerModule = ret.playerModule;
-    var baseModule = ret.baseModule;
+    // var layout = ret.layoutModule;
+    // var playerModule = ret.playerModule;
+    // var baseModule = ret.baseModule;
 
-    var canvasEl;
+    // var canvasEl;
     
-    layout.create();
+    // layout.create();
     // layout.canvas.refIndex['canvas'].appendChild(playerModule.view.el);
+    var tmpl = Trio.Renderer.createTemplate();
 
+    tmpl.create('table.test')
+            .create('tr#header')
+                .if('isShow')
+                    .create('td#if').append()
+                    .create('td#if').append()
+                    .create('td#if').append()
+                .else()
+                    .create('td.else').text('yoyoyo').append()
+                    .create('td.else').append()
+                    .create('td').addClass('else').removeClass('else').append()
+                .done()
+                .each('results')
+                    .create('td').text('hihi').append()
+                .done()
+            .append()
+        .append()
+
+    var el = tmpl.render({
+        isShow: false,
+        results: [1,2,3]
+    });
+    console.log(el, tmpl);
+
+    function isShow(d) {
+        return !d.isShow;
+    }
+
+    function results(d) {
+        return d.results;
+    }
 });
